463b53b1a6761e80504b46346ceda06cac8aa699
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-08-04 09:50:31 +0530

    profile and candidate routes

---
+++ public-api/routes/publicRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const publicController = require('../controllers/publicController');
+const router = express.Router();
+
+router.post('/public/profile', publicController.getProfile);
+router.get('/public/candidate', publicController.getCandidates);
+
+module.exports = router;
